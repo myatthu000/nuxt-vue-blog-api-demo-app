94f7e6c752e1a84a205daf3604ae1ea2c988ca05
myatthu000 <september.2019thu@gmail.com>
2023-09-13 22:49:28 +0630

    composables and middlerware

---
+++ middleware/auth.js
@@ -0,0 +1,11 @@
+export default defineNuxtRouteMiddleware((to, from) => {
+    
+    const {isLoggedIn} = useAuth();
+    if(process.client){
+        if(!isLoggedIn.value){
+
+            //   return navigateTo('/')
+            window.location.href = '/login'
+        } 
+    }
+})
